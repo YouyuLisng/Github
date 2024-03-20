fa3d8654119c2381e21d505ca2494ddee7bf4b41
YouyuLisng <a0979534311@gmail.com>
2024-03-20 21:17:19 +0800

    200403202117

--- app/actions/fetchRepos.ts
+++ app/actions/fetchRepos.ts
@@ -4,7 +4,7 @@ async function fetchRepos(pageNumber: number, perPage: number) {
         headers.append('Accept', 'application/vnd.github.v3+json');
 
         // 將 GitHub Personal Access Token 添加到請求中的 Authorization 標頭
-        headers.append('Authorization', 'Bearer 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');
+        // headers.append('Authorization', 'Bearer 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');
 
         const response = await fetch(`https://api.github.com/search/repositories?q=react&per_page=${perPage}&page=${pageNumber}`, {
             headers: headers
