b8a1658cdb456fa6cfa712c46b090b9cd5d29c05
YouyuLisng <a0979534311@gmail.com>
2024-03-24 03:45:22 +0800

    202403240345

--- components/GithubLogin.tsx
+++ components/GithubLogin.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function GithubLogin() {
     const handleGitHubLogin = () => {
         const clientId = '1d6c5925798aa7391380';
-        const redirectUri = 'http://localhost:3000/callback';
+        const redirectUri = '/callback';
         const scope = 'user';
         const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
 
