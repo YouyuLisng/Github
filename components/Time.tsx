ec7538e14e0e0484f6a2295413452037bc4ad69c
YouyuLisng <a0979534311@gmail.com>
2024-03-24 02:24:22 +0800

    202403240224

--- components/Time.tsx
+++ components/Time.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { format, parseISO } from 'date-fns';
+import { useState, useEffect } from 'react'
 
 interface TimeProps {
     time: string;
 }
 
 export default function Time({ time }: TimeProps) {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true)
+    }, []);
+
     const formatDate = (dateString: string) => {
         const date = parseISO(dateString);
         return format(date, 'yyyy-MM-dd');
@@ -13,7 +20,7 @@ export default function Time({ time }: TimeProps) {
 
     return (
         <>
-            <p className='text-sm text-zinc-500' suppressHydrationWarning={true}>{formatDate(time)}</p>
+            {isClient ? <p className='text-sm text-zinc-500' suppressHydrationWarning={true}>{formatDate(time)}</p> : null }
         </>
     )
 }
