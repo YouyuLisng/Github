44bf879dd4fba1d982c47c5502029f011331b0c0
YouyuLisng <a0979534311@gmail.com>
2024-03-25 18:08:55 +0800

    202403251808

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
+import { RepoList } from "@/components/RepoList/RepoList";
 import { Resizable } from "@/components/Resizable";
 import type { Metadata } from "next";
+import fetchRepos from "./actions/fetchRepos";
 
 export const metadata: Metadata = {
     title: "Dcard 2024 Web Frontend Intern Homework",
     description: "Dcard 2024 Web Frontend Intern Homework",
 };
 
-export default function Home() {
+export default async function Home() {
+    const repos = await fetchRepos(1);
     return (
         <>
-            <Resizable />
+            <RepoList repo={repos} />
         </>
     );
 }
