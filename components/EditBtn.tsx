588880291c1846c11a0f82a4439e87b8f92babe6
YouyuLisng <a0979534311@gmail.com>
2024-03-25 12:38:05 +0800

    202403251238

--- components/EditBtn.tsx
+++ components/EditBtn.tsx
@@ -6,7 +6,7 @@ export default function EditBtn() {
     return (
         <div className="fixed bottom-10 right-10 bg-sky-500 rounded-md cursor-pointer z-50">
             <div className='p-2'>
-                <FaEdit className='text-white' size={20} />123
+                <FaEdit className='text-white' size={20} />
             </div>
         </div>
     )
