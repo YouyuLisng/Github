a8817fae932fbdb006327f5af55f7592467362a0
YouyuLisng <a0979534311@gmail.com>
2024-03-30 19:09:22 +0800

    feat(route):308轉址 無用的路由刪除

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com'],
     },
+
+    async redirects() {
+        return [
+            {
+                source: '/user/:userName',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
