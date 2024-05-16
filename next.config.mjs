59c6d8b4374d9b629cf763feddbf26d7c3e53f1c
YouyuLisng <a0979534311@gmail.com>
2024-05-16 12:08:56 +0800

    update: 更新GITHUB_ACCESS_TOKEN next_config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         domains: ['avatars.githubusercontent.com'],
     },
     env: {
-        GITHUB_ACCESS_TOKEN: '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',
+        GITHUB_ACCESS_TOKEN: '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',
         GITHUB_ID: '17198fef42367fe66e07',
         GITHUB_SECRET: 'b3f482fa957cbf87e184bf15cb576c280f77961e',
     },
